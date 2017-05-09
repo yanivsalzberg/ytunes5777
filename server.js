c2356ed98b6d2c5efa1535e6545bdf814aa8e44f
yanivsalzberg1 <yanivsalzberg@gmail.com>
2017-05-09 17:17:20 +0300

    about to delete server

--- server.js
+++ server.js
@@ -1,41 +1,41 @@
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose');
+// var express = require('express');
+// var app = express();
+// var mongoose = require('mongoose');
 
-var bodyParser = require('body-parser')
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-mongoose.connect('mongodb://localhost/songs');
-
-
-app.use(express.static('public'));
-app.use(express.static('node_modules'));
-
-var Schema = mongoose.Schema;
-var songSchema = new Schema({
-  name: String,
-  notes: Array
-});
-
-var Song = mongoose.model('Song', songSchema);
-
-app.post("/songs", function(request, response){
-  console.log(request.body);//{title: jhkkjh,notes: [kj,kjkj,lkl]}
-
-  var tune = new Song()//{name: ,notes: }
-  tune.name = request.body.title;
-  tune.notes = request.body.notes;
-
-  tune.save();
-
-  Song.find().exec(function(err, songs){
-    response.send(songs);
-  })
+// var bodyParser = require('body-parser')
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+//
+// mongoose.connect('mongodb://localhost/songs');
 
 
-})
+// app.use(express.static('public'));
+// app.use(express.static('node_modules'));
 
-app.listen(8000, function() {
-  console.log("Serv's up, brah! 8000");
-});
+// var Schema = mongoose.Schema;
+// var songSchema = new Schema({
+//   name: String,
+//   notes: Array
+// });
+//
+// var Song = mongoose.model('Song', songSchema);
+
+// app.post("/songs", function(request, response){
+//   console.log(request.body);//{title: jhkkjh,notes: [kj,kjkj,lkl]}
+//
+//   var tune = new Song()//{name: ,notes: }
+//   tune.name = request.body.title;
+//   tune.notes = request.body.notes;
+//
+//   tune.save();
+//
+//   Song.find().exec(function(err, songs){
+//     response.send(songs);
+//   })
+//
+//
+// })
+//
+// app.listen(8000, function() {
+//   console.log("Serv's up, brah! 8000");
+// });
